f9905c5c5fdb8f60c541883d82f717936102bb78
shanellewiks <shanelle.wikramanayake.101@my.csun.edu>
2021-03-03 12:14:46 -0800

    Week 6 RMarkdown

---
+++ week6/Scripts/Week6_Lecture.R
@@ -0,0 +1,25 @@
+####Intro to R Markdown ###
+####Shanelle Wikramanayake###
+#### 2021-03-01####
+
+
+####Load libraries###
+library(tidyverse)
+library(here)
+library(lubridate)
+
+
+#parts of an R markdown doc: 
+#Metadata, 
+#Text, 
+#Code, 
+#Output
+
+library(beepr)
+beep(5)
+beep(9)
+beep(1)
+beep(2)
+beep(3)
+beep(6)
+beep(8)
